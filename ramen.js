faabe2c2cad752f2036450490f285ae95012307f
James <james@team-wombat.com>
2016-06-16 13:42:32 -0400

    reordered parameters

--- ramen.js
+++ ramen.js
@@ -6,37 +6,34 @@ var urltube;
 
 $(document).ready( function() {
 
-    var generateNonce = function(length) {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for(var i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-    }
-
     $("button").click(function(){
         var queryTerm = $("#keyword").val();
-        var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
+        // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
+        var url = "https://api.yelp.com/v2/search"
         var d = new Date();
         var n = d.getTime();
+        var generateNonce = function(length) {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for(var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
+        }
         var parameters = {
+            callback: 'cb',
+            jsonpCallback: 'cb',
             oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
-            oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
             oauth_nonce: generateNonce(16),
-            oauth_timestamp: n,
             oauth_signature: 'HGflok_6as2SEngYah75j61nwbw',
             oauth_signature_method: 'HMAC-SHA1',
-            oauth_version : '1.0',
-            callback: 'cb',
-            jsonpCallback: 'cb'
+            oauth_timestamp: n,
+            oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
+            oauth_version : '1.0'
     };
         $.ajax({
-            url: url,
             type: "GET",
-            // beforeSend: function (request) {
-            //     request.addHeader("Access-Control-Allow-Origin", "https://wombatdev.github.io");
-            // },
+            url: url,
             cache: true,
             data: parameters,
             dataType: "json"
