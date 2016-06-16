535c12d653cdc73194ef472c969ee86ff151cc76
James <james@team-wombat.com>
2016-06-16 13:54:14 -0400

    trying new key

--- ramen.js
+++ ramen.js
@@ -9,7 +9,7 @@ $(document).ready( function() {
     $("button").click(function(){
         var queryTerm = $("#keyword").val();
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
-        var url = "http://accesscontrolalloworiginall.herokuapp.com/https://api.yelp.com/v2/search"
+        var url = "https://api.yelp.com/v2/search"
         var d = new Date();
         var n = d.getTime();
         var generateNonce = function(length) {
@@ -25,11 +25,11 @@ $(document).ready( function() {
             jsonpCallback: 'cb',
             oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
             oauth_nonce: generateNonce(16),
-            oauth_signature: 'HGflok_6as2SEngYah75j61nwbw',
+            oauth_signature: 'qYdQ4oHcn9LNc826OafZ63iUH9M&HGflok_6as2SEngYah75j61nwbw',
             oauth_signature_method: 'HMAC-SHA1',
             oauth_timestamp: n,
             oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
-            oauth_version : '1.0'
+            oauth_version : '1.0',
     };
         $.ajax({
             type: "GET",
