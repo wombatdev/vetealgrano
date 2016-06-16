26e53bead0ff34abc9a4d515a57efe134fe620bc
James <james@team-wombat.com>
2016-06-16 13:44:04 -0400

    added heroku thing

--- ramen.js
+++ ramen.js
@@ -9,7 +9,7 @@ $(document).ready( function() {
     $("button").click(function(){
         var queryTerm = $("#keyword").val();
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
-        var url = "https://api.yelp.com/v2/search"
+        var url = "http://accesscontrolalloworiginall.herokuapp.com/https://api.yelp.com/v2/search"
         var d = new Date();
         var n = d.getTime();
         var generateNonce = function(length) {
