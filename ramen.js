34ab74c4889272a0995e6ce0f2f12fa1c723fb42
James <james@team-wombat.com>
2016-06-16 15:54:24 -0400

    went back to original failing state

--- ramen.js
+++ ramen.js
@@ -10,50 +10,41 @@ $(document).ready( function() {
         var queryTerm = $("#keyword").val();
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
 
-        var jso = new JSO({
-                providerID: "google",
-                client_id: "541950296471.apps.googleusercontent.com",
-                redirect_uri: "http://bridge.uninett.no/jso/index.html",
-                authorization: "https://accounts.google.com/o/oauth2/auth",
-                scopes: { request: ["https://www.googleapis.com/auth/userinfo.profile"]}
-            });
-})
+        var url = "https://api.yelp.com/v2/search"
+        var d = new Date();
+        var n = d.getTime();
+        var generateNonce = function(length) {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for(var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
+        }
+        var parameters = {
+            callback: 'cb',
+            jsonpCallback: 'cb',
+            oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
+            oauth_nonce: generateNonce(16),
+            oauth_signature: 'qYdQ4oHcn9LNc826OafZ63iUH9M&HGflok_6as2SEngYah75j61nwbw',
+            oauth_signature_method: 'HMAC-SHA1',
+            oauth_timestamp: n,
+            oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
+            oauth_version : '1.0',
+    };
+        $.ajax({
+            type: "GET",
+            url: url,
+            cache: true,
+            data: parameters,
+            dataType: "jsonp",
+        }).done(function(response) {
+            console.log(response);
 
-    //     var url = "https://api.yelp.com/v2/search"
-    //     var d = new Date();
-    //     var n = d.getTime();
-    //     var generateNonce = function(length) {
-    //         var text = "";
-    //         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    //         for(var i = 0; i < length; i++) {
-    //             text += possible.charAt(Math.floor(Math.random() * possible.length));
-    //         }
-    //         return text;
-    //     }
-    //     var parameters = {
-    //         callback: 'cb',
-    //         jsonpCallback: 'cb',
-    //         oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
-    //         oauth_nonce: generateNonce(16),
-    //         oauth_signature: 'qYdQ4oHcn9LNc826OafZ63iUH9M&HGflok_6as2SEngYah75j61nwbw',
-    //         oauth_signature_method: 'HMAC-SHA1',
-    //         oauth_timestamp: n,
-    //         oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
-    //         oauth_version : '1.0',
-    // };
-    //     $.ajax({
-    //         type: "GET",
-    //         url: url,
-    //         cache: true,
-    //         data: parameters,
-    //         dataType: "jsonp",
-    //     }).done(function(response) {
-    //         console.log(response);
-    //
-    //     }).fail(function(response) {
-    //         console.log("FAIL."+response)
-    //     })
-    // });
+        }).fail(function(response) {
+            console.log("FAIL."+response)
+        })
+    });
 
 
 
