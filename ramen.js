64589f2a22f9b66574a6aabb47703fed0586fdd5
James <james@team-wombat.com>
2016-06-16 13:02:02 -0400

    added callback thing

--- ramen.js
+++ ramen.js
@@ -17,7 +17,7 @@ $(document).ready( function() {
 
     $("button").click(function(){
         var queryTerm = $("#keyword").val();
-        var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen";
+        var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
         var d = new Date();
         var n = d.getTime();
         var parameters = {
@@ -33,9 +33,9 @@ $(document).ready( function() {
         $.ajax({
             url: url,
             type: "GET",
-            beforeSend: function (request) {
-                request.addHeader("Access-Control-Allow-Origin", "https://wombatdev.github.io");
-            },
+            // beforeSend: function (request) {
+            //     request.addHeader("Access-Control-Allow-Origin", "https://wombatdev.github.io");
+            // },
             cache: true,
             data: parameters,
             dataType: "json"
