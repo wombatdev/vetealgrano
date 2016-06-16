f81f930b77004e7dbd96ccf9dfc580794db557f4
James <james@team-wombat.com>
2016-06-16 15:26:07 -0400

    trying other method

--- ramen.js
+++ ramen.js
@@ -9,41 +9,78 @@ $(document).ready( function() {
     $("button").click(function(){
         var queryTerm = $("#keyword").val();
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
-        var url = "https://api.yelp.com/v2/search"
-        var d = new Date();
-        var n = d.getTime();
-        var generateNonce = function(length) {
-            var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            for(var i = 0; i < length; i++) {
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-            return text;
-        }
-        var parameters = {
-            callback: 'cb',
-            jsonpCallback: 'cb',
-            oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
-            oauth_nonce: generateNonce(16),
-            oauth_signature: 'qYdQ4oHcn9LNc826OafZ63iUH9M&HGflok_6as2SEngYah75j61nwbw',
-            oauth_signature_method: 'HMAC-SHA1',
-            oauth_timestamp: n,
-            oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
-            oauth_version : '1.0',
-    };
-        $.ajax({
-            type: "GET",
-            url: url,
-            cache: true,
-            data: parameters,
-            dataType: "jsonp"
-        }).done(function(response) {
-            console.log(response);
 
-        }).fail(function(response) {
-            console.log("FAIL."+response)
-        })
-    });
+
+        var oauth = OAuth({
+            consumer: {
+                public: 'J7vAHD6r4TPzvjnniyFoaw',
+                secret: 'qYdQ4oHcn9LNc826OafZ63iUH9M'
+            },
+            signature_method: 'HMAC-SHA1'
+        });
+        var request_data = {
+            url: 'https://api.yelp.com/v2/search',
+            method: 'GET',
+            dataType: "jsonp"
+        };
+        var token = {
+            public: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
+            secret: 'HGflok_6as2SEngYah75j61nwbw'
+        };
+
+        $.ajax({
+            url: request_data.url,
+            type: request_data.method,
+            data: oauth.authorize(request_data, token)
+        }).done(function(data) {
+            console.log(data)
+        }).fail(function(data) {
+            console.log("FAIL + "data)
+        });
+
+
+
+
+
+
+
+
+
+    //     var url = "https://api.yelp.com/v2/search"
+    //     var d = new Date();
+    //     var n = d.getTime();
+    //     var generateNonce = function(length) {
+    //         var text = "";
+    //         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    //         for(var i = 0; i < length; i++) {
+    //             text += possible.charAt(Math.floor(Math.random() * possible.length));
+    //         }
+    //         return text;
+    //     }
+    //     var parameters = {
+    //         callback: 'cb',
+    //         jsonpCallback: 'cb',
+    //         oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
+    //         oauth_nonce: generateNonce(16),
+    //         oauth_signature: 'qYdQ4oHcn9LNc826OafZ63iUH9M&HGflok_6as2SEngYah75j61nwbw',
+    //         oauth_signature_method: 'HMAC-SHA1',
+    //         oauth_timestamp: n,
+    //         oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
+    //         oauth_version : '1.0',
+    // };
+    //     $.ajax({
+    //         type: "GET",
+    //         url: url,
+    //         cache: true,
+    //         data: parameters,
+    //         dataType: "jsonp",
+    //     }).done(function(response) {
+    //         console.log(response);
+    //
+    //     }).fail(function(response) {
+    //         console.log("FAIL."+response)
+    //     })
+    // });
 
 
 
