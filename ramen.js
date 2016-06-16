ac54eaa1adc0c5ae9026d90b48d3df43e77459df
James <james@team-wombat.com>
2016-06-16 12:48:06 -0400

    added some stuff to try to fix authentication

--- ramen.js
+++ ramen.js
@@ -11,9 +11,21 @@ $(document).ready( function() {
         var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen";
         var d = new Date();
         var n = d.getTime();
+        var parameters = {
+            oauth_consumer_key: 'J7vAHD6r4TPzvjnniyFoaw',
+            oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
+            oauth_nonce: generateNonce(),
+            oauth_timestamp: n,
+            oauth_signature_method: 'HMAC-SHA1',
+            oauth_version : '1.0',
+            callback: 'cb',
+            jsonpCallback: 'cb'
+    };
         $.ajax({
             url: url,
             type: "GET",
+            cache: true,
+            data: parameters;
             dataType: "json"
         }).done(function(response) {
             console.log(response);
