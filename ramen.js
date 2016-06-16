8949ddd2eab4a57def93fa55ecee03b157091c1b
James <james@team-wombat.com>
2016-06-16 12:42:47 -0400

    switched to https

--- ramen.js
+++ ramen.js
@@ -8,7 +8,7 @@ $(document).ready( function() {
 
     $("button").click(function(){
         var queryTerm = $("#keyword").val();
-        var url = "http://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen";
+        var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen";
         var d = new Date();
         var n = d.getTime();
         $.ajax({
@@ -17,7 +17,7 @@ $(document).ready( function() {
             dataType: "json"
         }).done(function(response) {
             console.log(response);
-            
+
         }).fail(function(response) {
             console.log("FAIL."+response)
         })
