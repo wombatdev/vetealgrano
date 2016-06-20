b57b6ffb662ac920d492546da1e3c70da74b576a
James <james@team-wombat.com>
2016-06-20 14:52:56 -0400

    tbshooting

--- ramen.js
+++ ramen.js
@@ -9,7 +9,7 @@ var oauth_data;
 $(document).ready( function() {
 
     $("button").click(function(){
-        var queryTerm = $("#keyword").val();
+        var queryTerm = parseInt($("#keyword").val());
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
 
         var url = "https://api.yelp.com/v2/search?location="+queryTerm+"&term=ramen";
@@ -33,11 +33,11 @@ $(document).ready( function() {
             signature_method: 'HMAC-SHA1'
         });
         var request_data = {
-            url: 'https://api.yelp.com/v2/search?term=ramen',
+            url: 'https://api.yelp.com/v2/search?term=ramen&location=20001',
             method: 'POST',
-            data: {
-                location: queryTerm
-            }
+            // data: {
+            //     location: queryTerm
+            // }
         };
         var token = {
             public: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
