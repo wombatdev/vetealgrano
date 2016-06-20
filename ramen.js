f488fb8b7a1e6e29559e68e2ace82aa0baa89ab2
James <james@team-wombat.com>
2016-06-20 14:15:52 -0400

    made oauth a constructor

--- ramen.js
+++ ramen.js
@@ -30,7 +30,7 @@ $(document).ready( function() {
         //     oauth_token: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
         //     oauth_version : '1.0',
         // };
-        var oauth = OAuth({
+        var oauth = new OAuth({
             consumer: {
                 public: 'J7vAHD6r4TPzvjnniyFoaw',
                 secret: 'qYdQ4oHcn9LNc826OafZ63iUH9M'
@@ -48,6 +48,16 @@ $(document).ready( function() {
             public: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
             secret: 'HGflok_6as2SEngYah75j61nwbw'
         };
+
+        var oauth_data = {
+        oauth_consumer_key: oauth.consumer.public,
+        oauth_nonce: generateNonce(),
+        oauth_signature_method: oauth.signature_method,
+        oauth_timestamp: n,
+        oauth_version: '1.0',
+        oauth_token: token.public
+        };
+
         $.ajax({
             url: request_data.url,
             type: request_data.method,
