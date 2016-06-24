d9c864f64053e5995e82d4b952b483fd03d571f8
James <james@team-wombat.com>
2016-06-24 16:35:27 -0400

    testing

--- ramen.js
+++ ramen.js
@@ -13,7 +13,7 @@ $(document).ready( function() {
         debugger
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
 
-        var search_url = "https://api.yelp.com/v2/search?term=ramen";
+        var search_url = "https://api.yelp.com/v2/search?";
         var getStamp = function () {
             var d = new Date();
             return d.getTime();
