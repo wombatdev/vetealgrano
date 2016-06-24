735f30a1b607e4bec92d4a56c2013552a26ee438
James <james@team-wombat.com>
2016-06-24 16:36:54 -0400

    reversion

--- ramen.js
+++ ramen.js
@@ -10,10 +10,9 @@ $(document).ready( function() {
 
     $("button").click(function(){
         var queryTerm = parseInt($("#keyword").val());
-        debugger
         // var url = "https://api.yelp.com/v2/search?oauth_consumer_key=J7vAHD6r4TPzvjnniyFoaw&location="+queryTerm+"&oauth_nonce=gobblygook&oauth_signature=HGflok_6as2SEngYah75j61nwbw&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1466094519984&oauth_token=u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc&term=ramen&callback=?";
 
-        var search_url = "https://api.yelp.com/v2/search?";
+        var url = "https://api.yelp.com/v2/search?location="+queryTerm+"&term=ramen";
         var getStamp = function () {
             var d = new Date();
             return d.getTime();
@@ -34,8 +33,12 @@ $(document).ready( function() {
             signature_method: 'HMAC-SHA1'
         });
         var request_data = {
-            url: search_url,
+            url: 'https://api.yelp.com/v2/search?',
             method: 'POST',
+            data: {
+                'location': queryTerm,
+                'term': "ramen"
+            }
         };
         var token = {
             public: 'u5PmZDSUDdZcj2wk7oW9yH_k1BO7c3fc',
